e7102c5e876f0065e81a969b7d41bedbc9cb6b56
8jkesh <162061123+8jkesh@users.noreply.github.com>
2024-03-06 08:57:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['23459080129', '🅿️layfulm3️⃣😌❤️', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
